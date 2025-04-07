b3c90bc0c28a0f8338eaf524f2979288e2652319
joaolira-dev <joaovictorlira06@hotmail.com>
2025-04-07 08:24:30 -0300

    Startando API

---
+++ src/main/java/dev/java10x/Backend/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.Backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+   
+   @GetMapping("/home")
+   public String welcome() {
+      return "Essa é minha primeira mensagem";
+   }
+
+}
